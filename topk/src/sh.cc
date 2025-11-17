1bc6ecf830c2a9d84c3afa5de3b153e3549a41f6
Patrick Dinklage <patrick.dinklage@tu-dortmund.de>
2025-11-17 10:07:56 +0100

    Output to file and load only prefix of file

--- topk/src/sh.cc
+++ topk/src/sh.cc
@@ -22,6 +22,7 @@
 #include <ctime>
 #include <vector>
 #include <cmath>
+#include <limits>
 #include <omp.h>
 #include <sys/time.h>
 #include <tuple>
@@ -34,15 +35,8 @@
 
 using namespace std;
 
-void output_top_k(unsigned char * sequence, vector<pair<int64_t,uint32_t>> &topK, string filename) {
-    /*
-    ofstream of(filename, ios::out);
-    if (!of) {
-        cerr << "Error opening file: " << filename << endl;
-        exit(1);
-    }
-    */
-    
+template<typename Out>
+void output_top_k(unsigned char * sequence, vector<pair<int64_t,uint32_t>> &topK, Out& out) {
     fp::RabinKarp61 rk(257);
 
     for (auto &u : topK) {
@@ -50,28 +44,29 @@ void output_top_k(unsigned char * sequence, vector<pair<int64_t,uint32_t>> &topK
 		for(uint32_t j=0; j<u.second;j++)
             fp = rk.push(fp, sequence[u.first+j]);
 
-		cout << fp << "," << u.second << "," << 0 << endl;
+		out << fp << "," << u.second << "," << 0 << endl;
     }
-    // of.close();
 }
 
 int main(int argc, char* argv[])
 {
 	if(argc<3)
 	{	
-		cerr << "Usage: " << argv[0] << " text-file " << " K " << endl;
+		cerr << "Usage: " << argv[0] << " text-file " << " K " << " [outfile] " << " [prefix] " << endl;
 		return -1;
 	}
 	
 	INT K = atoll(argv[2]);
 	int64_t L = 1;
+    
+    INT max_n = (argc > 4) ? atoll(argv[4]) : std::numeric_limits<INT>::max();
 	
 	ifstream seq(argv[1], ios::in | ios::binary);
     	unsigned char * input_seq_char = NULL;
 	unsigned char * sequence;
 	INT n = 0;
     	char c;
-    	while (seq.get(c))
+    	while (n < max_n && seq.get(c))
     	{
        		if(n == 0 || n % ALLOC_SIZE)	input_seq_char = ( unsigned char * ) realloc ( input_seq_char,   ( n + ALLOC_SIZE ) * sizeof ( unsigned char ) );
         	input_seq_char[n] = c;
@@ -99,7 +94,15 @@ int main(int argc, char* argv[])
 
 	std::chrono::steady_clock::time_point  stream_end = std::chrono::steady_clock::now();
 	cerr<<"Top-K computation time: "<< std::chrono::duration_cast<std::chrono::milliseconds>(stream_end - stream_begin).count() << "[ms]." << std::endl;	
-	output_top_k(sequence,topK, string(argv[1]) + "_sh_top_K" + to_string(K));
+    
+    if(argc > 3) {
+        ofstream of(argv[3]);
+        output_top_k(sequence,topK, of);
+        of.close();
+    } else {
+        output_top_k(sequence,topK, cout);
+    }
+	
 	free (sequence);
 	return 0;
 }
